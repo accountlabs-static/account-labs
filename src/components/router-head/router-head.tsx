a01e8499060e5e76212a682e7a660dc622823577
Charon-Fan <Charon.Dian@gmail.com>
2023-05-19 17:49:54 +0800

    feat: implement Analytics & update totem

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -24,6 +24,14 @@ export const RouterHead = component$(() => {
       {head.styles.map((s) => (
         <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2BLK2J0D2X"></script>
+      <script dangerouslySetInnerHTML={`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-2BLK2J0D2X');
+        `}
+      />
     </>
   );
 });
