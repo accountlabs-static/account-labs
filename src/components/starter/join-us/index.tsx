a0a39e904238515e5912757a91cfc2893543b324
Charon-Fan <Charon.Dian@gmail.com>
2023-08-14 09:28:22 +0800

    feat: adjust join us section

--- src/components/starter/join-us/index.tsx
+++ src/components/starter/join-us/index.tsx
@@ -11,7 +11,7 @@ export default component$(() => {
       <Logo height="7.5rem" width="7.5rem" class="mt-20 mb-2" />
       <div class="h-0.5 w-18 mb-8" style="background: linear-gradient(90deg, rgba(90, 204, 143, 0.16) 0%, rgba(90, 204, 143, 0.8) 50%, rgba(90, 204, 143, 0.16) 100%);" />
       <div class="mb-4 bg-clip-text text-transparent bg-gradient-to-r from-title-range-start to-title-range-end select-none text-2xl font-normal">Join us</div>
-      <div class="w-2/3 mx-auto mb-10 leading-6 select-none text-center tracking-wider" style="color: rgba(255, 255, 255, 0.56);">We're constantly searching for exceptional individuals who align with our principles and can aid us in advancing our objectives. Simply send us an email explaining why you think you'd be an excellent addition to our team.</div>
+      <div class="w-2/3 mx-auto mb-10 leading-6 select-none text-center tracking-wider" style="color: rgba(255, 255, 255, 0.56);">We're constantly searching for exceptional individuals who align with our principles and can aid us in advancing our objectives. Click the button below to learn more about us and view our open positions.</div>
       <div style="--animation-speed:4s" class={`${styles['glowing-box']} ${styles['glowing-box-active']} mb-10`}>
         <div class={`${styles['glowing-box-animations']}`}>
           <div class={`${styles['glowing-box-glow']}`} />
@@ -25,12 +25,10 @@ export default component$(() => {
         <button
           class="flex select-none px-6 py-4" style="border: 1px solid rgba(255,255,255,.1); border-radius: 999px"
           onClick$={() => {
-            const recipient = "talents@accountlabs.com";
-            const attachment = "http://example.com/file.pdf";
-            window.location.href = `mailto:${recipient}?attachment=${attachment}`;
+            window.open('/careers', '_blank');
           }}
         >
-          <span>talents@accountlabs.com</span>
+          <span>Join Us Now</span>
         </button>
       </div>
     </div>
